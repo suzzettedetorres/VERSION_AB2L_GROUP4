1a18c6510a75c3c9e3cd4dfe7b8b49cf07f115d1
willgrace <graycelsantos@gmail.com>
2013-02-04 17:48:20 +0800

    UI Prototype
    Sample main page for our basic UI

---
+++ UI/Main.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Main</title>
+<link href="style.1.2.1.css" media="screen" rel="stylesheet" type="text/css" />
+<link href="Main.css" media="screen" rel="stylesheet" type="text/css" />
+
+	<link rel="stylesheet" href="../themes/default/default.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="../themes/light/light.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="../themes/dark/dark.css" type="text/css" media="screen" />
+   	<link rel="stylesheet" href="../themes/bar/bar.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="../nivo-slider.css" type="text/css" media="screen" />
+    <link rel="stylesheet" href="style.css" type="text/css" media="screen" />
+   
+	
+</head>
+
+<body>	
+	<div id="navigation">
+    	<div id="div1"> 
+                <div class="tile bg-color-blue icon">                	
+                      <img src="img/Email.png" />                   
+                </div>
+                <div class="tile bg-color-blue">
+                	<img src="img/rec.png" />
+                </div>
+            	<div class="tile bg-color-blue">
+                	<img src="img/win.png" />
+                </div>
+           		<div class="tile bg-color-blue">
+                <img src="img/note.png" />
+                </div>
+    	</div>
+        <div id="div2">
+        	<div class="tile double-vertical bg-color-red"><img src="img/sample.png" /></div>
+        </div>
+        <div id="div3">        	
+        	<div class="tile double double-vertical bg-color-greenDark"><img src="img/touch.png" /></div>
+        </div>
+        <div id="div4">        	
+            <div class="tile bg-color-greenLight"><img src="img/Search.png" /></div>
+           	<div class="tile bg-color-blue"><img src="img/delete.png" /></div>
+        </div>
+    </div>
+    <div id="Main">
+        <div id="slide">
+        	 <div id="wrapper">        
+                <div class="slider-wrapper theme-default">
+                      <div id="slider" class="nivoSlider">
+                                <img src="images/women.jpg" data-thumb="images/women.jpg" alt="" title="#htmlcaption1"/>
+                             	<img src="images/women1.jpg" data-thumb="images/women1.jpg" alt="" title="#htmlcaption2" />
+                                <img src="images/women2.jpg" data-thumb="images/women2.jpg" alt="" title="#htmlcaption3" />
+                                <img src="images/uplb.jpg" data-thumb="images/uplb.jpg" alt="" title="#htmlcaption4" />
+                            </div>
+                            <!--<div id="htmlcaption1" class="nivo-html-caption">
+                               <a href="http://cf.ph.gameclub.com/landing/index.html"><small>Explore and PLAY</small></a>. 
+                            </div>
+                            <div id="htmlcaption2" class="nivo-html-caption">
+                               <a href="http://suddenattack.nexon.net/"><small>Explore and PLAY</small></a>. 
+                            </div>
+                            <div id="htmlcaption3" class="nivo-html-caption">
+                                <a href="http://blog.dota2.com/"><small>Explore and PLAY</small></a>. 
+                            </div>
+                             <div id="htmlcaption4" class="nivo-html-caption">
+                                 <a href="http://sea.battle.net/sc2/en/"><small>Explore and PLAY</small></a>. 
+                            </div>-->
+                </div>
+        
+            </div>
+            <script type="text/javascript" src="scripts/jquery-1.7.1.min.js"></script>
+            <script type="text/javascript" src="../jquery.nivo.slider.js"></script>
+            <script type="text/javascript">
+            $(window).load(function() {
+                $('#slider').nivoSlider();
+            });
+            </script>
+        </div>
+         <div class="titulo"><b>Women's Residence Hall Dormers Management System</b> </div>
+    	<div id="banner">This web-based database management system for Women's Residence Hall, one of the dormitories in the University of the Philippines Los Baños will keep track of the records and data of the dormers including residents' personal information, contact details, payment records, reservation records, violation records, and records on usage of furniture, appliances, equipment and other resources.</div>
+    </div>
+
+</body>
+
+</html>
+
+
+
+
+<!--References:
+
+Retrived at 2 February, 2013 from
+http://simoborto.altervista.org
+https://uho.uplb.edu.ph/index.php
+http://danielrichardeviant.deviantart.com/
+http://dev7studios.com/
+-->
